25805ee2dd2e44ed31b2a2c6abe61bc23e918163
Daiana Valeria Casas <38088617+DaianaCasas@users.noreply.github.com>
2023-07-04 09:49:51 -0300

    Add: response message

--- Ejercicio2/server.c
+++ Ejercicio2/server.c
@@ -13,10 +13,10 @@
 
 int main(int argc, char * argv[]){
   int listendFd=0, connFd=0;
-
+  int n=0;
   struct sockaddr_in server_addr;
   char sendMsg[1025];
-  char srecMsg[1025];
+  char recMsg[1025];
   time_t ticks;
 
   listendFd = socket(AF_INET, SOCK_STREAM,0);
@@ -39,18 +39,18 @@ int main(int argc, char * argv[]){
   while(1){
     connFd = accept(listendFd, (struct sockaddr*)NULL, NULL); 
     ticks = time(NULL);
-    if ((n = read(listendFd, srecMsg, sizeof(srecMsg)-1)) > 0){
-      srecMsg[n] = 0;
-      if(fputs(srecMsg, stdout) == EOF)
-      {
-          printf("\n Error : Fputs error\n");
-      }
-      snprintf(sendMsg,sizeof(sendMsg), "%s\r\n","021000");
-      write(connFd, sendMsg, strlen(sendMsg)); 
+    read(connFd, recMsg, strlen(recMsg)); 
+    fputs(recMsg, stdout);
+    if(n < 0)
+    {
+        printf("\n Sin respuesta \n");
     }
+    printf("%s",recMsg);
+    snprintf(sendMsg,sizeof(sendMsg), "%s\r\n","021000");
+    write(connFd, sendMsg, strlen(sendMsg));
+    printf("\nSolicitud procesada\n");
     close(connFd);
     sleep(1);
   }
   return 0;
 }
-
